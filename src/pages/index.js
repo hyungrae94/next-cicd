a06b4190e0335dbb72a6c4b27cd53d1db81d0e90
우형래 <rea1109@squares.ai>
2023-04-04 16:35:34 +0900

    트랙패드

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
      <main>
-      <h1>폼 미쳤다.</h1>
+      <h1> 트랙패드 안삼</h1>
      </main>
     </>
   )
